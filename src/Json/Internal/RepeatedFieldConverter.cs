839e2f111ea482bdd2c912a15b05c261cb1a3591
Ronny Gunawan <3048897+ronnygunawan@users.noreply.github.com>
2021-10-08 01:40:56 +0700

    Rename project

--- src/Json/Internal/RepeatedFieldConverter.cs
+++ src/Json/Internal/RepeatedFieldConverter.cs
@@ -1,9 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using Google.Protobuf.Collections;
 
-namespace RG.System.Text.Json.ProtobufSerializer.Internal {
+namespace RG.ProtobufConverters.Json.Internal {
+	internal class RepeatedFieldConverter : JsonConverterFactory {
+		private static readonly Dictionary<Type, JsonConverter> ConverterByElementType = new();
+		private static readonly object Gate = new();
+		public static readonly RepeatedFieldConverter Instance = new();
+
+		private RepeatedFieldConverter() { }
+
+		public override bool CanConvert(Type typeToConvert) => typeToConvert.IsGenericType
+			&& typeToConvert.GetGenericTypeDefinition() == typeof(RepeatedField<>);
+
+		public override JsonConverter CreateConverter(Type typeToConvert, JsonSerializerOptions options) {
+			Type elementType = typeToConvert.GetGenericArguments()[0];
+			lock (Gate) {
+				if (!ConverterByElementType.TryGetValue(elementType, out JsonConverter? converter)) {
+					converter = (JsonConverter)Activator.CreateInstance(typeof(RepeatedFieldConverter<>).MakeGenericType(elementType), options);
+					ConverterByElementType.Add(elementType, converter);
+				}
+				return converter;
+			}
+		}
+	}
+
 	internal class RepeatedFieldConverter<T> : JsonConverter<RepeatedField<T>> {
 		private readonly JsonConverter<T> _valueConverter;
 
---
+++ src/Json/ProtobufJsonConverter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Google.Protobuf;
+using RG.ProtobufConverters.Json.Internal;
+
+namespace RG.ProtobufConverters.Json {
+	public class ProtobufJsonConverter : JsonConverterFactory {
+		private static readonly Dictionary<Type, JsonConverter> ConverterByMessageType = new();
+		private static readonly object Gate = new();
+		public static readonly ProtobufJsonConverter Instance = new();
+
+		public static readonly JsonSerializerOptions Options = new() {
+			Converters = {
+				Instance,
+				RepeatedFieldConverter.Instance
+			}
+		};
+
+		private ProtobufJsonConverter() { }
+
+		public override bool CanConvert(Type typeToConvert) => !typeToConvert.IsInterface
+			&& !typeToConvert.IsAbstract
+			&& typeof(IMessage).IsAssignableFrom(typeToConvert);
+
+		public override JsonConverter CreateConverter(Type typeToConvert, JsonSerializerOptions options) {
+			lock (Gate) {
+				if (ConverterByMessageType.TryGetValue(typeToConvert, out JsonConverter? converter)) {
+					converter = (JsonConverter)Activator.CreateInstance(typeof(ProtobufJsonConverter<>).MakeGenericType(typeToConvert), options);
+					ConverterByMessageType.Add(typeToConvert, converter);
+				}
+				return converter;
+			}
+		}
+	}
+
+	internal class ProtobufJsonConverter<T> : JsonConverter<T> {
+		private readonly JsonConverter<T> _valueConverter;
+
+		public ProtobufJsonConverter(JsonSerializerOptions options) {
+			_valueConverter = (JsonConverter<T>)options.GetConverter(typeof(T));
+		}
+
+
+	}
+}
